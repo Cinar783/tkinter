19f5f77d63f0cb7dab0dd82232e9fb22520bb605
Seyithan Çınar <seyithan.cinarr@gmail.com>
2023-11-02 08:16:15 +0300

    tinter revision

--- tkinterexample.py
+++ tkinterexample.py
@@ -2,8 +2,16 @@ import tkinter
 
 
 window=tkinter.Tk()
+window.title("Python Tkinter")
 
 
+my_label=tkinter.Label(text='This is label',foreground='Black',font=("Arial",30,"normal"))
+window.minsize(width=500,height=500)
+my_label.pack()
+
+
+my_button=tkinter.Button(text="My button",background="Black",foreground="White",font=("Arial",20,"italic"))
+my_button.pack()
 
 
 tkinter.mainloop()
