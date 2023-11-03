1f3de6a86b7b1d98f6ac6824ebaa86ed6cb72eeb
Seyithan Çınar <seyithan.cinarr@gmail.com>
2023-11-03 18:36:23 +0300

    tkinter added row and column

--- tkinterexample.py
+++ tkinterexample.py
@@ -10,18 +10,18 @@ def button_funcion():
 #label
 my_label=tkinter.Label(text='This is label',foreground='Black',font=("Arial",30,"normal"))
 window.minsize(width=500,height=500)
-#my_label.pack(side='top')
-my_label.place(x=40,y=20)
-
+#my_label.place(x=40,y=20)
+my_label.grid(row=1,column=1)
 
 #button
 my_button=tkinter.Button(text="My button",command=button_funcion)
-#my_button.pack(side="top")
-
+#my_button.place(x=100,y=100)
+my_button.grid(row=2,column=1)
 
 #entry
 my_entry=tkinter.Entry(width=50,)
-#my_entry.pack(side="left")
+#my_entry.place(x=40,y=150)
+my_entry.grid(row=3,column=2)
 
 
 
